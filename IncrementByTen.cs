0f99a3e7873c7f4b16d54346b89a573d1f129b04
yamgarcia <yamgarcia.ca@gmail.com>
2022-08-16 09:16:54 -0700

    Initial Commit

---
+++ IncrementByTen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace WorkFlowExtension
+{
+    public class IncrementByTen : CodeActivity
+    {
+
+        [RequiredArgument]
+        [Input("Decimal input")]
+        public InArgument<decimal> DecInput { get; set; }
+
+        [Output("Decimal output")]
+        public OutArgument<decimal> DecOutput { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            decimal input = DecInput.Get(context);
+            DecOutput.Set(context, input + 10);
+        }
+
+    }
+}
